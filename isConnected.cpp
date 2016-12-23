ed4d08cf627f61f27f878e99f8f260a9d55093a5
Swapnil Saxena <swapnilsaxena@live.in>
2016-12-24 00:07:04 +0530

    Update isConnected.cpp

--- isConnected.cpp
+++ isConnected.cpp
@@ -4,24 +4,24 @@
  * Source: https://github.com/dragonslayerx 
  */
 
-int isConnected(vector<vector<int> > &G, int u){
+int isConnected(vector<vector<int> > &G, int src){
 	bool isVisited[MAX] = {false};
 	queue<int> Q;
-	Q.push(u);
+	Q.push(src);
 	while (Q.size()) {
 		int u = Q.front();
-		isVisited[u] = true;
 		Q.pop();
-		for (vector<int>::iterator V = G[u].begin(); V != G[u].end(); V++) {
-			if (!isVisited[*V]) {
-				Q.push(*V);
+		isVisited[u] = true;
+		for (int v : G[u]) {
+			if (!isVisited[v]) {
+				Q.push(v);
 			}
 		}
 	}
 	for (int i = 0; i < G.size(); i++) {
        		if (isVisited[i] == false) {
             		return false;
-        		}
+        	}
 	}
 	return true;
 }
