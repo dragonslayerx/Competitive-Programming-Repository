27b80eed1548b30d83f1ab3a961cbb8b9f48e2c0
Swapnil Saxena <swapnilsaxena@live.in>
2016-12-24 00:13:36 +0530

    Update rmq simple.cpp

--- rmq simple.cpp
+++ rmq simple.cpp
@@ -1,19 +1,16 @@
 /**
  * Description: Range Min Query using Sparse Table
- * Usage: construct O(NlgN), query O(lg(N)) / O(1)
+ * Usage: construct O(NlgN), query O(lg(N))
  * Source: https://github.com/dragonslayerx 
  */
 
-#include <iostream>
-using namespace std;
-
-#define MAX 1000000
-#define size 25
+const int MAX = 1000000;
+const int size = 25;
+    
 int rmq[size][MAX];
 int a[MAX];
 
-void construct(int n)
-{
+void construct(int n){
     for (int j = 0; j < n; j++)
         rmq[0][j] = a[j];
     for (int i = 1; i < size; i++) {
@@ -25,43 +22,24 @@ void construct(int n)
     }
 }
 
-int query(int l, int r)
-{
+/**
+ * Usage: query O(lg(N))
+ */
+
+int query(int l, int r){
     int length = r-l+1, p = 0;
     while ((1 << p) <= length) p++;
     p--;
     return max(rmq[p][l], rmq[p][r-(1<<p)+1]);
 }
 
-int main()
-{
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    construct(n);
-}
-
-// --------------- O(1) RMQ --------------------------
-
-int rmq[SIZE][MAX];
-
-void construct(int *a, int n)
-{
-    for (int j = 0; j < n; j++)
-        rmq[0][j] = a[j];
-    for (int i = 1; i < SIZE; i++) {
-        int length = 1<<(i-1);
-        for (int j = 0; j < n; j++) {
-            if (j+length > n) rmq[i][j] = min(rmq[i-1][j], 0);
-            else rmq[i][j] = min(rmq[i-1][j], rmq[i-1][j+length]);
-        }
-    }
-}
+/**
+ * Usage: preprocess O(N), query O(1)
+ * Note: Call preprocess function before using query function. 
+ */
 
 int frameSize[MAX];
-int preprocess()
-{
+int preprocess(){
     for(int i=0, pow2=1; pow2 < MAX;  pow2*=2, i++)
         frameSize[pow2]=i;
     for(int i=3;i<MAX;i++) {
@@ -71,8 +49,7 @@ int preprocess()
     }
 }
 
-inline int query(int rmq[][MAX], int l, int r)
-{
+inline int query(int rmq[][MAX], int l, int r){
     int p = frameSize[r-l+1];
     return min(rmq[p][l], rmq[p][r-(1<<p)+1]);
 }
