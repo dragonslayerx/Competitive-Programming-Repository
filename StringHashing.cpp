e853fa4fefcd756db3a6e6d5a5793ff6cf173e92
Swapnil Saxena <swapnilsaxena@live.in>
2017-05-10 13:59:35 +0530

    Update StringHashing.cpp

--- StringHashing.cpp
+++ StringHashing.cpp
@@ -1,5 +1,5 @@
 /**
- * Description: Dynamic Segment tree (Support for inbetween insertion and array reversal)   
+ * Description: Dynamic String Hashing 
  * Usage: initalize O(N), getHash O(n), getSubHash O(1), getRevSubHash O(1) 
  * Note: In case double hashing is required http://codeforces.com/contest/710/submission/20094958
  * Source: https://github.com/dragonslayerx 
