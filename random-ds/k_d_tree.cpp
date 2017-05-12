72503a54cfbb2e8220264c0f0b3f620852e5ab20
Swapnil Saxena <swapnilsaxena@live.in>
2017-05-13 00:17:42 +0530

    Update k_d_tree.cpp

--- random-ds/k_d_tree.cpp
+++ random-ds/k_d_tree.cpp
@@ -52,7 +52,7 @@ class KDTree
         for (int i = 0; i < N; i++) {
             dist += (a[i] - b[i]) * (a[i] - b[i]);
         }
-        return pow(dist, (double)1/N);
+        return pow(dist, 0.5);
     }
 
     Node* root;
